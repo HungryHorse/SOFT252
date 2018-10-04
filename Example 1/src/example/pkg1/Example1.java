e01448ef1ef6d236d82add0e53cf6ef2ad6cb6dc
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-10-04 15:07:52 +0100

    Extracted zip

---
+++ Example 1/src/example/pkg1/Example1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example.pkg1;
+
+/**
+ *
+ * @author jjbrewer
+ */
+public class Example1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World"); 
+        System.out.println("The date today is " + 22);
+        System.out.println(true); 
+        System.exit(0);
+    }
+    
+}
